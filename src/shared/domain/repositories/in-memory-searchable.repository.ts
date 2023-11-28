5c649ec5849e080dc3d17fb94cdec5d8fc6cc338
Henrique Silva Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-11-28 15:43:49 -0300

    Adding applyPaginate method in InMemorySearchableRepository

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -26,13 +26,24 @@ export abstract class InMemorySearchableRepository<E extends Entity>
 
     protected abstract applyFilter(items: E[], filter: string | null): Promise<E[]>
 
-    protected async applySort(items: E[], sort: string | null, sortDir: string | null): E[] {
-        if (!sort || !sortDir) {
-            return items
-        }
+    protected async applySort(items: E[], sort: string | null, sortDir: string | null): Promise<E[]> {
+        if (!sort || !this.sortableFields.includes(sort)) return items
+
+        return [...items].sort((a, b) => {
+            if (a.props[sort] < b.props[sort]) return sortDir === 'asc' ? -1 : 1
+            if (a.props[sort] > b.props[sort]) return sortDir === 'asc' ? 1 : -1
+            return 0
+        })
     }
 
-    protected async applyPaginate(items: E[], page: SearchParams['page'], perPage: SearchParams['perPage']): E[] {
-        return items.slice((page - 1) * perPage, page * perPage)
+    protected async applyPaginate(
+        items: E[],
+        page: SearchParams['page'],
+        perPage: SearchParams['perPage'],
+    ): Promise<E[]> {
+        const start = (page - 1) * perPage
+        const end = start + perPage
+
+        return items.slice(start, end)
     }
 }
