28427566b4cdc3fceee5bbae7d4e9102972961ee
Henrique Silva Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-12-14 13:42:27 -0300

    UserPrismaRepository - Implementing findById

--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -1,6 +1,8 @@
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
 import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.service'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserRepository } from '@/users/domain/repository/user.repository'
+import { UserModelMapper } from '../models/user-model.mapper'
 
 export class UserPrismaRepository implements UserRepository.Repository {
     sortableFields: string[]
@@ -10,6 +12,7 @@ export class UserPrismaRepository implements UserRepository.Repository {
     findByEmail(email: string): Promise<UserEntity> {
         throw new Error('Method not implemented.')
     }
+
     emailExists(email: string): Promise<void> {
         throw new Error('Method not implemented.')
     }
@@ -17,19 +20,38 @@ export class UserPrismaRepository implements UserRepository.Repository {
     search(input: UserRepository.SearchParams): Promise<UserRepository.SearchResult> {
         throw new Error('Method not implemented.')
     }
+
     insert(entity: UserEntity): Promise<void> {
         throw new Error('Method not implemented.')
     }
-    findById(id: string): Promise<UserEntity> {
-        throw new Error('Method not implemented.')
+
+    async findById(id: string): Promise<UserEntity> {
+        return this._get(id)
     }
+
     findAll(): Promise<UserEntity[]> {
         throw new Error('Method not implemented.')
     }
+
     update(entity: UserEntity): Promise<void> {
         throw new Error('Method not implemented.')
     }
+
     delete(id: string): Promise<void> {
         throw new Error('Method not implemented.')
+    }
+
+    protected async _get(id: string): Promise<UserEntity> {
+        try {
+            const user = await this.prismaService.user.findUnique({
+                where: {
+                    id,
+                },
+            })
+
+            return UserModelMapper.toEntity(user)
+        } catch (err) {
+            throw new NotFoundError(`User not found using ID ${id}`)
+        }
     }
 }
