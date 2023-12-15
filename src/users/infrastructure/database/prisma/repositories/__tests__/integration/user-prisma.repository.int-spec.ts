552c4da06024f3c8785c9347300ef1bf61112abd
Henrique Silva Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-12-15 16:01:38 -0300

    Integration testes of Search p1 - UserPrismaRepository

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -6,6 +6,8 @@ import { DatabaseModule } from '@/shared/infrastructure/database/database.module
 import { NotFoundError } from '@/shared/domain/errors/not-found-error'
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repository/user.repository'
+import { SearchResult } from '@/shared/domain/repositories/searchable-repository-contracts'
 
 describe('UserPrismaRepository Integration tests', () => {
     const prismaService = new PrismaClient()
@@ -57,6 +59,37 @@ describe('UserPrismaRepository Integration tests', () => {
         expect(entities).toHaveLength(1)
         entities.map(item => {
             expect(item.toJSON()).toStrictEqual(entity.toJSON())
+        })
+    })
+
+    describe('Search methods', () => {
+        it('Should apply only pagination when the other params are null', async () => {
+            const createdAt = new Date()
+            const entities: UserEntity[] = []
+            const arrange = Array(16).fill(UserDataBuilder({}))
+
+            arrange.forEach((item, index) => {
+                entities.push(
+                    new UserEntity({
+                        ...item,
+                        name: `User${index}`,
+                        email: `teste${index}@email.com`,
+                        createdAt: new Date(createdAt.getTime() + index),
+                    }),
+                )
+            })
+
+            await prismaService.user.createMany({
+                data: entities.map(entity => entity.toJSON()),
+            })
+
+            const SearchOutput = await sut.search(new UserRepository.SearchParams())
+
+            expect(SearchOutput).toBeInstanceOf(UserRepository.SearchResult)
+            expect(SearchOutput.total).toBe(16)
+            SearchOutput.items.forEach(item => {
+                expect(item).toBeInstanceOf(UserEntity)
+            })
         })
     })
 })
