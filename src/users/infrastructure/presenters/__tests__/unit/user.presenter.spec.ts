78cb98cb62406ad78252cbd60eb9e367454007f2
Henrique Silva Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-12-19 08:45:18 -0300

    UsersPresenter unit tests - Part 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,6 +1,8 @@
+import { instanceToPlain } from 'class-transformer'
 import { UserPresenter } from '../../user.presenter'
 
 describe('UsersPresenter unit tests', () => {
+    let sut: UserPresenter
     let props = {
         id: '775878ef-2675-4672-b9d3-818565e87ec5',
         name: 'any_name',
@@ -9,14 +11,27 @@ describe('UsersPresenter unit tests', () => {
         createdAt: new Date(),
     }
 
+    beforeEach(() => {
+        sut = new UserPresenter(props)
+    })
+
     describe('Contructor method', () => {
-        const sut = new UserPresenter(props)
         it('should be defined', () => {
             expect(sut.id).toEqual(props.id)
             expect(sut.name).toEqual(props.name)
             expect(sut.email).toEqual(props.email)
             expect(sut.createdAt).toEqual(props.createdAt)
             expect(sut['password']).toBeUndefined()
+        })
+    })
+
+    it('should present data', () => {
+        const output = instanceToPlain(sut)
+        expect(output).toMatchObject({
+            id: props.id,
+            name: props.name,
+            email: props.email,
+            createdAt: props.createdAt.toISOString(),
         })
     })
 })
