ddaba1d0f6e627ad0c4782d43ab891d3cea73642
Henrique Silva Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-02 12:32:17 -0300

    refect: remove user e2e tests

--- src/users/infrastructure/__tests__/e2e/removeUser.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/removeUser.e2e-spec.ts
@@ -13,6 +13,8 @@ import { applyGlobalConfig } from '@/global-config'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { HashProviderInterface } from '@/shared/application/providers/hash-provider.interface'
+import { BcryptjsHashProvider } from '../../providers/hash-provider/bcryptjs-hash.provider'
 
 describe('UsersController e2e tests', () => {
     let app: INestApplication
@@ -21,6 +23,10 @@ describe('UsersController e2e tests', () => {
     const prismaService = new PrismaClient()
 
     let entity: UserEntity
+
+    let hashProvider: HashProviderInterface
+    let hashPassword: string
+    let acessToken: string
 
     beforeAll(async () => {
         // setupPrismaTests()
@@ -34,27 +40,55 @@ describe('UsersController e2e tests', () => {
         await app.init()
 
         repository = module.get<UserRepository.Repository>('UserRepository')
+        hashProvider = new BcryptjsHashProvider()
+        hashPassword = await hashProvider.generateHash('1234')
     })
 
     beforeEach(async () => {
         await prismaService.user.deleteMany()
-        entity = new UserEntity(UserDataBuilder({}))
+
+        entity = new UserEntity(UserDataBuilder({ email: 'a@a.com', password: hashPassword }))
         await repository.insert(entity)
+
+        const loginRes = await request(app.getHttpServer())
+            .post('/users/login')
+            .send({
+                email: 'a@a.com',
+                password: '1234',
+            })
+            .expect(200)
+
+        acessToken = loginRes.body.acessToken
     })
 
     describe('DELETE /users/:id', () => {
         it('Should delete a user', async () => {
-            await request(app.getHttpServer()).delete(`/users/${entity.id}`).expect(204).expect({})
+            await request(app.getHttpServer())
+                .delete(`/users/${entity.id}`)
+                .set('Authorization', `Bearer ${acessToken}`)
+                .expect(204)
+                .expect({})
 
             await expect(repository.findById(entity.id)).rejects.toThrow(NotFoundError)
         })
 
         it('Should return a error with 404 code when throw NotFoundError with invalid id', async () => {
-            const res = await request(app.getHttpServer()).delete(`/users/fakeId`).expect(404).expect({
-                statusCode: 404,
-                error: 'Not Found',
-                message: 'User not found using ID fakeId',
-            })
+            const res = await request(app.getHttpServer())
+                .delete(`/users/fakeId`)
+                .set('Authorization', `Bearer ${acessToken}`)
+                .expect(404)
+                .expect({
+                    statusCode: 404,
+                    error: 'Not Found',
+                    message: 'User not found using ID fakeId',
+                })
+        })
+
+        it('Should return a error with 401 code when the request is unauthorized', async () => {
+            const res = await request(app.getHttpServer())
+                .delete(`/users/fakeId`)
+                .expect(401)
+                .expect({ message: 'Unauthorized', statusCode: 401 })
         })
     })
 })
